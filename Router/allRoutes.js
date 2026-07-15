b1bdd25f4aed599ddcb41a917e7160b09b4de911
champion <jphilippe.champion@gmail.com>
2026-07-15 23:48:13 +0200

    html and js of signup, signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,9 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "accueil", "/pages/home.html"),
 new Route("/contact", "contact", "/pages/contact.html", "js/contact.js"),
-new Route("/connexion", "connexion", "/pages/connexion.html", "/js/connexion.js"),
-new Route("/signup", "signup", "/pages/signup.html", "/js/signup.js"),
-new Route("/menus", "menus", "/pages/menus.html", "/js/menus.js"),];
+new Route("/signin", "signin", "/pages/signin.html", "js/connect.js"),
+new Route("/signup", "signup", "/pages/signup.html", "js/signup.js"),
+new Route("/menus", "menus", "/pages/menus.html", "js/menus.js"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "vite et gourmand";
