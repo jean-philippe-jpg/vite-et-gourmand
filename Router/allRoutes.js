ae31edcb57c61e372bf17c4c317665728de0723a
champion <jphilippe.champion@gmail.com>
2026-07-15 23:09:59 +0200

    mise à jour du routage et index.html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,11 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "accueil", "/pages/home.html"),
+new Route("/contact", "contact", "/pages/contact.html", "js/contact.js"),
+new Route("/connexion", "connexion", "/pages/connexion.html", "/js/connexion.js"),
+new Route("/signup", "signup", "/pages/signup.html", "/js/signup.js"),
+new Route("/menus", "menus", "/pages/menus.html", "/js/menus.js"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "vite et gourmand";
