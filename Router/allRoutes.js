1eddb8c378d85f2687dcd9b99765976e43644b5a
champion <jphilippe.champion@gmail.com>
2026-07-16 10:14:44 +0200

    html page menus et details menus

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,9 @@ export const allRoutes = [
 new Route("/contact", "contact", "/pages/contact.html", "js/contact.js"),
 new Route("/signin", "signin", "/pages/signin.html", "js/connect.js"),
 new Route("/signup", "signup", "/pages/signup.html", "js/signup.js"),
-new Route("/menus", "menus", "/pages/menus.html", "js/menus.js"),];
+new Route("/menus", "menus", "/pages/menus.html", "js/menus.js"),
+new Route("/detail-menu", "detail-menu", "/pages/detail-menus.html", "js/menus.js"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "vite et gourmand";
