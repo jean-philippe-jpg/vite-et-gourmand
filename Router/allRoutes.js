355f62b80799fa066baec0e4c84df11dd9cd7aea
champion <jphilippe.champion@gmail.com>
2026-07-20 23:45:24 +0200

    mise a jour du responsive des menus

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,19 +3,19 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "accueil", "/pages/home.html"),
-<<<<<<< HEAD
+/*<<<<<<< HEAD
 new Route("/contact", "contact", "/pages/contact.html", "js/auth/contact.js"),
 new Route("/signin", "signin", "/pages/auth/signin.html", "js/auth/signin.js"),
 new Route("/signup", "signup", "/pages/auth/signup.html", "js/auth/signup.js"),
 new Route("/menus", "menus", "/pages/menus.html", "js/auth/menus.js"),];
-=======
++ =======*/
 new Route("/contact", "contact", "/pages/contact.html", "js/contact.js"),
 new Route("/signin", "signin", "/pages/signin.html", "js/connect.js"),
 new Route("/signup", "signup", "/pages/signup.html", "js/signup.js"),
 new Route("/menus", "menus", "/pages/menus.html", "js/menus.js"),
 new Route("/detail-menu", "detail-menu", "/pages/detail-menus.html", "js/menus.js"),
 ];
->>>>>>> menus
+//>>>>>>> menus
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "vite et gourmand";
